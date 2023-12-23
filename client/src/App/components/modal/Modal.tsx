de46cff57a7c73ccb38e2b229ab6a39c197a0a18
juystin <nguyen.2274@osu.edu>
2023-12-23 16:55:24 -0600

    Added drop shadow to modal; fixed click-through

--- client/src/App/components/modal/Modal.tsx
+++ client/src/App/components/modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ModalTypes } from "../../types/ModalTypes";
 import ClassroomModal from "./ClassroomModal";
 import DefaultModal from "./DefaultModal";
@@ -19,16 +19,22 @@ const Modal = ({ type, activeMarker, setActiveMarker, buildingData, availability
     const [activeClassroom, setActiveClassroom] = useState<string | null>(null)
     const [isOpen, setIsOpen] = useState<boolean>(true)
 
+    useEffect(() => {
+        if (activeMarker !== 0) {
+            setIsOpen(true)
+        }
+    }, [activeMarker])
+
     return ( isOpen ?
-        <div style={{position: "absolute", zIndex: "2", right: 0, width: "40%", height: "100%", display: "flex", alignItems: "center"}}>
+        <div style={{position: "absolute", zIndex: "2", right: 0, width: "40%", height: "100%", display: "flex", alignItems: "center", filter: "drop-shadow(15px 0px 30px #13070C)", pointerEvents: "none"}}>
             <div style={{width: "5%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <div style={{height: "80px", width: "100%", background: "#BA0C2F", display: "flex", alignItems: "center", justifyContent: "center", borderTopLeftRadius: "8px", borderBottomLeftRadius: "8px"}}
+                <div style={{height: "80px", width: "100%", background: "#BA0C2F", display: "flex", alignItems: "center", justifyContent: "center", borderTopLeftRadius: "8px", borderBottomLeftRadius: "8px", cursor: "pointer", position: "relative", zIndex: 2, pointerEvents: "auto"}}
                     onClick={() => setIsOpen(!isOpen)}
                 >
                     <ArrowIcon isOpen={isOpen}/>
                 </div>
             </div>
-            <div style={{width: "95%", height: "100%", overflow: "scroll", background: "#EEE5E9"}}>
+            <div style={{width: "95%", height: "100%", overflow: "scroll", background: "#EEE5E9", pointerEvents: "auto"}}>
             {
                 type === ModalTypes.ALL 
                 ?
@@ -47,9 +53,9 @@ const Modal = ({ type, activeMarker, setActiveMarker, buildingData, availability
             </div>
         </div>
         :
-        <div style={{position: "absolute", zIndex: "2", right: 0, width: "40%", height: "100%", display: "flex", alignItems: "center", justifyContent: "flex-end"}}>
+        <div style={{position: "absolute", zIndex: "2", right: 0, width: "40%", height: "100%", display: "flex", alignItems: "center", justifyContent: "flex-end", filter: "drop-shadow(0px 0px 30px #13070C)", pointerEvents: "none"}}>
             <div style={{width: "5%", height: "100%", display: "flex", alignItems: "center", justifyContent: "center"}}>
-                <div style={{height: "80px", width: "100%", background: "#BA0C2F", display: "flex", alignItems: "center", justifyContent: "center", borderTopLeftRadius: "8px", borderBottomLeftRadius: "8px"}}
+                <div style={{height: "80px", width: "100%", background: "#BA0C2F", display: "flex", alignItems: "center", justifyContent: "center", borderTopLeftRadius: "8px", borderBottomLeftRadius: "8px", cursor: "pointer", pointerEvents: "auto"}}
                     onClick={() => setIsOpen(!isOpen)}
                 >
                     <ArrowIcon isOpen={isOpen}/>
